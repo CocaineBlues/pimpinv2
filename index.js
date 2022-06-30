8fc64939e7753d3965f24730c8784117c1d3bdfe
CocaineBlues <mihaifurnicag@gmail.com>
2022-06-30 17:16:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+i was drippin in school nigga i was drippin oooooooouhhh, smoking that strong spice botana blana
+
+console.log("hello, world!");
+
+
+console.log("sossy sossy bitch im bossy flossy uuuuuuhhh");
